c7e718fcf1404eb3984db0ef316c75bce398bb36
Cassinix <ninicassini@gmail.com>
2019-03-14 23:16:13 +0300

    feat: add check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let roundBracketsCount = 0;
+  let curlyBracketsCount = 0;
+  let squareBracketsCount = 0;
+  let verticalLineCount = 0;
+  for (let i = 0; i < str.length; i++){
+         if (str[i] === '(') roundBracketsCount++;
+    else if (str[i] === ')') roundBracketsCount--;
+    else if (str[i] === '[') squareBracketsCount++;
+    else if (str[i] === ']') squareBracketsCount--;
+    else if (str[i] === '{') curlyBracketsCount++;
+    else if (str[i] === '}') curlyBracketsCount--;
+    else if (str[i] === '|') roundBracketsCount++;
+    else if (str[i] === '|') roundBracketsCount--;
+  
+   const result = 
+    roundBracketsCount === 0 && 
+    squareBracketsCount === 0 &&
+    curlyBracketsCount === 0 &&
+    verticalLineCount === 0;
+  return result;
+}
 }
